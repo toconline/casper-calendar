1e2bc8074962c262c134d785f22d7e084da591aa
AndreSilva1993 <andreemanuel.1993@gmail.com>
2019-12-09 18:04:52 +0000

    When the year changes, maintain the active date and active date range / initial implementation of the holidays

--- casper-calendar.js
+++ casper-calendar.js
@@ -12,6 +12,15 @@ class CasperCalendar extends PolymerElement {
   static get properties () {
     return {
       /**
+       * The global application's app object.
+       *
+       * @type {Object}
+       */
+      app: {
+        type: Object,
+        value: window.app
+      },
+      /**
        * The year that is currently being displayed on the calendar.
        *
        * @type {Number}
@@ -41,6 +50,17 @@ class CasperCalendar extends PolymerElement {
         type: Object,
         observer: '__activeDateRangeChanged',
         notify: true
+      },
+      __holidays: {
+        type: Array,
+        value: () => {
+          return [
+            { name: 'Natal', date: new Date(2019, 11, 25) },
+            { name: 'Dia de Implementação da República', date: new Date(2019, 9, 5) },
+            { name: 'Revolução 25 de Abril', date: new Date(2019, 3, 25) },
+            { name: 'Dia de Portugal', date: new Date(2019, 5, 10) },
+          ];
+        }
       },
       /**
        * This array contains the cells that are currently painted in the page.
@@ -75,6 +95,8 @@ class CasperCalendar extends PolymerElement {
           align-items: center;
           justify-content: center;
           border-radius: 3px;
+          font-size: 14px;
+          position: relative;
           box-sizing: border-box;
           border-left: 1px #F2F2F2 solid;
           border-bottom: 1px #F2F2F2 solid;
@@ -88,6 +110,11 @@ class CasperCalendar extends PolymerElement {
         #main-container .row-container .cell:not(.cell--left-header):not(.cell--top-header):hover {
           cursor: pointer;
           box-shadow: 1px 1px 7px #999999;
+        }
+
+        #main-container .row-container .cell.cell--today {
+          color: white;
+          background-color: orange;
         }
 
         #main-container .row-container .cell.cell--weekend {
@@ -130,6 +157,25 @@ class CasperCalendar extends PolymerElement {
           color: red;
           background-color: #E4E4E4;
         }
+
+        #main-container .row-container .cell .holiday {
+          right: 0;
+          bottom: 0;
+          position: absolute;
+          width: 13px;
+          height: 13px;
+          padding-top: 1px;
+          padding-left: 1px;
+          font-size: 8px;
+          line-height: 13px;
+          color: white;
+          display: flex;
+          opacity: 0.75;
+          box-sizing: border-box;
+          border-radius: 12px 3px 3px 3px;
+          justify-content: center;
+          background-color: #FF5000;
+        }
       </style>
       <div id="main-container">
         <div class="row-container">
@@ -146,18 +192,24 @@ class CasperCalendar extends PolymerElement {
           </template>
         </div>
 
-        <template is="dom-repeat" items="[[__months]]" as="month">
+        <template is="dom-repeat" items="[[__months]]" as="month" id="templateRepeat">
           <div class="row-container">
             <div class="cell cell--left-header">[[month.name]]</div>
             <template is="dom-repeat" items="[[__getDaysForMonth(index)]]" as="monthDay">
               <div
+                data-year$="[[year]]"
                 data-month$="[[month.index]]"
                 data-day$="[[monthDay.index]]"
                 on-click="__cellClicked"
                 on-mouseup="__cellOnMouseUp"
                 on-mousedown="__cellOnMouseDown"
                 on-mouseenter="__cellOnMouseEnter"
-                class$="cell [[__isWeekend(monthDay.weekDay)]]">[[monthDay.index]]</div>
+                class$="cell [[__isWeekend(monthDay.weekDay)]]">
+                  [[monthDay.index]]
+                  <template is="dom-if" if="[[__isHoliday(month.index, monthDay.index)]]">
+                    <span tooltip="[[__holidayTooltip(month.index, monthDay.index)]]" class="holiday">F</span>
+                  </template>
+                </div>
             </template>
           </div>
         </template>
@@ -168,18 +220,86 @@ class CasperCalendar extends PolymerElement {
   ready () {
     super.ready();
 
+    this.addEventListener('mousemove', event => this.app.tooltip.mouseMoveToolip(event));
     this.addEventListener('mouseout', () => {
       if (this.__isUserSelectingRange) {
         this.__cellOnMouseUp();
       }
     });
+
+    this.$.templateRepeat.addEventListener('dom-change', () => {
+      afterNextRender(this, () => {
+        this.__paintTodayCell();
+        this.__paintActiveDateCell(true);
+        this.__paintActiveDateRangeCells(true);
+      });
+    });
   }
 
-  __yearChanged () {
+  /**
+   * This method paints the cell that represents the present day.
+   */
+  __paintTodayCell () {
+    const today = new Date();
+    if (today.getFullYear() !== this.year) return;
+
+    const todayCell = this.__findCellByYearMonthAndDay(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+
+    if (todayCell) todayCell.classList.add('cell--today');
+  }
+
+  /**
+   * This method paints the current active date cell.
+   *
+   * @param {Boolean} wasCalendarRedrawn This flag indicates if the method was invoked after the calendar was redrawn or not.
+   */
+  __paintActiveDateCell (wasCalendarRedrawn) {
+    if (!wasCalendarRedrawn) this.__removeActiveAttributeFromCells();
+
+    if (!this.activeDate || this.activeDate.getFullYear() !== this.year) return;
+
+    const activeDateCell = this.__findCellByYearMonthAndDay(
+      this.activeDate.getFullYear(),
+      this.activeDate.getMonth(),
+      this.activeDate.getDate()
+    );
+
+    if (activeDateCell) {
+      this.__activeCells.push(activeDateCell);
+      activeDateCell.setAttribute('active', '');
+    }
+  }
+
+  /**
+   * This method paints the current active date range cells.
+   *
+   * @param {Boolean} wasCalendarRedrawn This flag indicates if the method was invoked after the calendar was redrawn or not.
+   */
+  __paintActiveDateRangeCells (wasCalendarRedrawn) {
+    if (!wasCalendarRedrawn) this.__removeActiveAttributeFromCells();
+
+    if (!this.activeDateRange) return;
+
+    this.__paintDateRangeCells(
+      moment(this.activeDateRange.startRange),
+      moment(this.activeDateRange.endRange)
+    );
+  }
+
+  /**
+   * This method gets invoked as soon as the year changes.
+   *
+   * @param {Number} year The current year.
+   */
+  __yearChanged (year) {
     const months = [];
     this.__numberOfColumns = 31;
 
-    const yearFirstWeekDay = new Date(this.year, 0, 1).getDay();
+    const yearFirstWeekDay = new Date(year, 0, 1).getDay();
 
     for (let monthIndex = 0; monthIndex < 12; monthIndex++) {
       months.push({
@@ -188,7 +308,7 @@ class CasperCalendar extends PolymerElement {
       });
 
       // Get the month's number of days and its first week day.
-      const firstDayOfMonth = moment(new Date(this.year, monthIndex, 1));
+      const firstDayOfMonth = moment(new Date(year, monthIndex, 1));
       const monthNumberOfDays = firstDayOfMonth.daysInMonth();
       const monthFirstWeekDay = firstDayOfMonth.day();
 
@@ -267,6 +387,14 @@ class CasperCalendar extends PolymerElement {
    */
   __isWeekend (weekDay) {
     return weekDay === 0 || weekDay === 6 ? 'cell--weekend' : '';
+  }
+
+  __isHoliday (month, day) {
+    return this.__holidays.some(holiday => holiday.date.getMonth() === month && holiday.date.getDate() === day);
+  }
+
+  __holidayTooltip (month, day) {
+    return this.__holidays.find(holiday => holiday.date.getMonth() === month && holiday.date.getDate() === day).name;
   }
 
   /**
@@ -349,16 +477,8 @@ class CasperCalendar extends PolymerElement {
       return;
     }
 
-    const currentDateCell = this.__findCellByMonthAndDay(activeDate.getMonth(), activeDate.getDate());
-
-    // This means the calendar is not yet fully rendered, so we postpone the remaining function.
-    if (!currentDateCell) return afterNextRender(this, () => this.__activeDateChanged(activeDate));
-
-    this.__removeActiveAttributeFromCells();
     this.__internallyChangeActiveDateRange(undefined);
-
-    currentDateCell.setAttribute('active', true);
-    this.__activeCells.push(currentDateCell);
+    this.__paintActiveDateCell();
   }
 
   /**
@@ -372,17 +492,10 @@ class CasperCalendar extends PolymerElement {
     }
 
     // This means the active date range was changed internally.
-    if (this.__activeDateRangeLock) {
-      this.__activeDateRangeLock = false;
-      return;
-    }
+    if (this.__activeDateRangeLock) return;
 
-    this.__removeActiveAttributeFromCells();
     this.__internallyChangeActiveDate(undefined);
-    this.__paintDateRangeCells(
-      moment(activeDateRange.startRange),
-      moment(activeDateRange.endRange)
-    );
+    this.__paintActiveDateRangeCells();
   }
 
   /**
@@ -391,8 +504,8 @@ class CasperCalendar extends PolymerElement {
    * @param {Number} month The month that will be used in the cell's selector.
    * @param {Number} day The day that will be used in the cell's selector.
    */
-  __findCellByMonthAndDay (month, day) {
-    return this.shadowRoot.querySelector(`.cell[data-month="${month}"][data-day="${day}"]`);
+  __findCellByYearMonthAndDay (year, month, day) {
+    return this.shadowRoot.querySelector(`.cell[data-year="${year}"][data-month="${month}"][data-day="${day}"]`);
   }
 
   /**
@@ -410,6 +523,8 @@ class CasperCalendar extends PolymerElement {
    * @param {Date} rangeEnd The range's end date.
    */
   __paintDateRangeCells (rangeStart, rangeEnd) {
+    if (rangeStart.year() > this.year || rangeEnd.year() < this.year) return;
+
     const daysBetweenBothDates = rangeEnd.diff(rangeStart, 'days');
 
     for (let daysCount = 0; daysCount <= Math.abs(daysBetweenBothDates); daysCount++) {
@@ -417,9 +532,19 @@ class CasperCalendar extends PolymerElement {
         ? moment(rangeStart).add(daysCount, 'days')
         : moment(rangeStart).subtract(daysCount, 'days');
 
-      const currentDateCell = this.__findCellByMonthAndDay(currentDate.month(), currentDate.date());
-      currentDateCell.setAttribute('active', true);
-      this.__activeCells.push(currentDateCell);
+      // If a date interval spans over several years and the current date is not displayed, skip to the next iteration.
+      if (currentDate.year() !== this.year) continue;
+
+      const currentDateCell = this.__findCellByYearMonthAndDay(
+        currentDate.year(),
+        currentDate.month(),
+        currentDate.date()
+      );
+
+      if (currentDateCell) {
+        currentDateCell.setAttribute('active', '');
+        this.__activeCells.push(currentDateCell);
+      }
     }
   }
 
@@ -431,6 +556,7 @@ class CasperCalendar extends PolymerElement {
   __internallyChangeActiveDate (activeDate) {
     this.__activeDateLock = true;
     this.activeDate = activeDate;
+    this.__activeDateLock = false;
   }
 
   /**
@@ -441,6 +567,7 @@ class CasperCalendar extends PolymerElement {
   __internallyChangeActiveDateRange (activeDateRange) {
     this.__activeDateRangeLock = true;
     this.activeDateRange = activeDateRange;
+    this.__activeDateRangeLock = false;
   }
 }
 
