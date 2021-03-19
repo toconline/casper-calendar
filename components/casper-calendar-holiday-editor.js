6e233ede9c1f17c96584791a970de2e42b156db8
Rita Silva <arita.lcas@gmail.com>
2021-03-19 11:23:16 +0000

    Holiday editor now has create and edit modes

--- components/casper-calendar-holiday-editor.js
+++ components/casper-calendar-holiday-editor.js
@@ -9,6 +9,18 @@ import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, PolymerElement) {
   static get properties () {
     return {
+      description: {
+        type: String,
+        value: ''
+      },
+      editorMode: {
+        type: String,
+        value: 'Criar'
+      },
+      isCreate: {
+        type: String,
+        value: true
+      }
     };
   }
 
@@ -24,62 +36,12 @@ class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, 
           max-width: 300px;
           width: 300px;
           max-height: 300px;
-          height: 150px;
+          height: 146px;
         }
 
         paper-listbox {
           border-radius: var(--radius-primary, 6px);
           padding: 0px;
-        }
-
-        #descriptionInput {
-          width: 90%;
-          margin: 0 auto;
-        }
-
-        .delete {
-          color: var(--primary-color);
-          padding: 4px 10px;
-          border-radius: 20%;
-          border: 2px solid var(--primary-color);
-          margin-right: 5px;
-        }
-
-        .accept {
-          color: white;
-          padding: 4px 10px;
-          border-radius: 20%;
-          background: var(--primary-color);
-          border: 2px solid var(--primary-color);
-        }
-
-        .container {
-          display: flex;
-          width: 90%;
-          margin: 0 auto;
-          justify-content: flex-end;
-          margin-top: 15px;
-        }
-
-        .delete-alt {
-          color: var(--primary-color);
-          font-size: 13px;
-          display: flex;
-          align-items: center;
-          margin-right: 10px;
-          padding: 4px 14px 4px 14px;
-          border-radius: 15px;
-          border: 1.5px solid var(--primary-color);
-        }
-
-        .accept-alt {
-          color: white;
-          font-size: 13px;
-          display: flex;
-          align-items: center;
-          padding: 4px 14px 4px 14px;
-          border-radius: 15px;
-          background: var(--primary-color);
         }
 
         .header {
@@ -88,12 +50,9 @@ class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, 
           padding: 7px 0;
         }
 
-        /* cinza*/
-        /* .header {
-          background: #efefef;
-          color: var(--primary-color);
-          padding: 7px 0;
-        } */
+        .header:focus {
+          outline: none;
+        }
 
         .header-content {
           width: 90%;
@@ -105,46 +64,77 @@ class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, 
         .close {
           width: 20px;
           height: 20px;
+          cursor: pointer;
+        }
+
+        #descriptionInput {
+          width: 90%;
+          margin: auto;
+        }
+
+        .buttons-container {
+          display: flex;
+          width: 90%;
+          margin: auto;
+          justify-content: flex-end;
+          margin-top: 10px;
+        }
+
+        .buttons-container:focus {
+          outline: none;
+        }
+
+        /* .button {
+          font-size: 13px;
+          display: flex;
+          align-items: center;
+          padding: 4px 14px 4px 14px;
+          border-radius: 15px;
+          cursor: pointer;
+        }
+
+        .delete-button {
+          color: var(--primary-color);
+          margin-right: 10px;
+          border: 1.5px solid var(--primary-color);
+        }
+
+        .accept-button {
+          color: white;
+          background: var(--primary-color);
+        } */
+
+        .button {
+          padding: 4px 14px 4px 14px;
+          border-radius: 15px;
+          cursor: pointer;
+          border: 1.5px solid var(--primary-color);
+        }
+
+        .delete-button {
+          color: var(--primary-color);
+          margin-right: 10px;
+        }
+
+        .accept-button {
+          color: white;
+          background: var(--primary-color);
         }
       </style>
 
-      <!-- <paper-listbox>
-        <casper-menu-separator>
-          Editar Feriado
-          <casper-icon class="close" icon="fa-light:times" on-click="_o"></casper-icon>
-        </casper-menu-separator>
-        <paper-input id="descriptionInput" label="Descrição"></paper-input>
-        <div class="container">
-          <casper-icon class="delete" icon="fa-light:trash-alt" on-click="_o"></casper-icon>
-          <casper-icon class="accept" icon="fa-light:check" on-click="_o"></casper-icon>
-        </div>
-      </paper-listbox> -->
-      <!-- <paper-listbox>
-        <casper-menu-separator>
-          Editar Feriado
-          <casper-icon class="close" icon="fa-light:times" on-click="_o"></casper-icon>
-        </casper-menu-separator>
-        <paper-input id="descriptionInput" label="Descrição"></paper-input>
-        <div class="container">
-          <casper-icon class="delete-alt" icon="fa-light:trash-alt" on-click="_o"></casper-icon>
-          <casper-icon class="accept-alt" icon="fa-light:check" on-click="_o"></casper-icon>
-        </div>
-      </paper-listbox> -->
       <paper-listbox>
-        <!-- <casper-menu-separator>
-          Editar Feriado
-          <casper-icon class="close" icon="fa-light:times" on-click="_o"></casper-icon>
-        </casper-menu-separator> -->
         <div class="header">
           <div class="header-content">
-            <span >Editar Feriado</span>
-            <casper-icon class="close" icon="fa-light:times-circle" on-click="_o"></casper-icon>
+            <span>[[editorMode]] Feriado</span>
+            <casper-icon class="close" icon="fa-light:times-circle" on-click="_closeEditor"></casper-icon>
           </div>
         </div>
-        <paper-input id="descriptionInput" width="300px" label="Descrição"></paper-input>
-        <div class="container">
-          <span class="delete-alt"><casper-icon icon="fa-light:trash-alt" on-click="_o"></casper-icon></span>
-          <span class="accept-alt"><casper-icon icon="fa-light:check" on-click="_o"></casper-icon></span>
+        <paper-input id="descriptionInput" width="300px" label="Descrição" value="{{description}}"></paper-input>
+        <div class="buttons-container">
+          <template is="dom-if" if="[[!isCreate]]">
+            <casper-icon icon="fa-light:trash-alt" class="button delete-button" on-click="_o"></casper-icon>
+          </template>
+          <casper-icon icon="fa-light:check" class="button accept-button" on-click="_acceptHoliday"></casper-icon>
         </div>
       </paper-listbox>
     `;
@@ -157,6 +147,12 @@ class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, 
   ready () {
     super.ready();
 
+    if (this.editorMode === 'Criar') {
+      this.isCreate = true;
+    } else if (this.editorMode === 'Editar') {
+      this.isCreate = false;
+    }
+
     this.addEventListener('opened-changed', (event) => {
       if (event && event.detail && event.detail.value) {
         afterNextRender(this, () => {
@@ -166,7 +162,24 @@ class CasperCalendarHolidayEditor extends mixinBehaviors(CasperOverlayBehavior, 
     });
   }
 
+  _closeEditor () {
+    this.close();
+  }
+
+  _acceptHoliday () {
+    this.dispatchEvent(new CustomEvent('casper-holiday-editor-accept-description', {
+      bubbles: true,
+      composed: true,
+      detail: { description: this.description }
+    }));
+
+    this.close();
+  }
+
 }
   
 customElements.define(CasperCalendarHolidayEditor.is, CasperCalendarHolidayEditor);
+
+
+// officeBranchesWiz.$.casperCalendar.activeDates
   
